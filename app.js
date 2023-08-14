b6b74864d970a67b14db5bd4aee893e8417171be
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-08-15 02:41:48 +0530

    ewtr

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import Connection from "./config/db.js";
+import userRouter from "./controllers/userController.js";
+
+const port = 8080;
+
+const app = express();
+
+app.use(express.json());
+
+// database
+Connection(
+  "mongodb+srv://nitesh0709yadav:nitesh0709yadav@cluster0.ctp026m.mongodb.net/?retryWrites=true&w=majorityesrew"
+);
+
+app.use("/", userRouter);
+
+
+try {
+  app.listen(port, () => {
+    console.log(`app server running on ${port}...`);
+  });
+} catch (err) {
+  console.log(err);
+}
